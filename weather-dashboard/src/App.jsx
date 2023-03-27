e00eae57b1b9407ea3fa6bd25caff17a8d468d63
Truc Duong <trucdg@bu.edu>
2023-03-27 19:59:36 -0400

    add header section and its styling

--- weather-dashboard/src/App.jsx
+++ weather-dashboard/src/App.jsx
@@ -4,7 +4,14 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1> Hello world</h1>
+      <header>
+        <h2>
+          <i className="fa-solid fa-cloud-sun-rain"></i> The Weather App
+        </h2>
+      </header>
+      <div className="main-page">
+        <h1> Welcome to the Weather App!</h1>
+      </div>
     </div>
   );
 }
